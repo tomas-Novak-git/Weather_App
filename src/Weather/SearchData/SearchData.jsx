07441a65aec194422e12d060f7ab5d19ca436444
tomas-novak-git <tomas_novak@centrum.cz>
2024-04-01 14:03:04 +0200

    data fetch debug

---
+++ src/Weather/SearchData/SearchData.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import styles from '../SearchData/SearchData.module.css'
+
+const SearchData = ({data}) => {
+  return (
+    <div className={styles.dataDiv}>
+        
+    </div>
+  )
+}
+
+export default SearchData
