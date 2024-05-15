af04f568a76c779674178de92161264eaa96e9a6
tomas-novak-git <tomas_novak@centrum.cz>
2024-05-15 18:47:29 +0200

    icon add

---
+++ src/components/Weather/InfoCard.jsx
@@ -0,0 +1,15 @@
+import {
+  IoRainyOutline,
+  IoThunderstormOutline,
+  IoSnow,
+  IoCloudyOutline,
+  IoSunnyOutline,
+  IoPartlySunnyOutline,
+} from "react-icons/io5";
+import { RiMistFill } from "react-icons/ri";
+
+const InfoCard = ({ icon, heading }) => {
+  return <div>InfoCard</div>;
+};
+
+export default InfoCard;
